c70fc66290858d1951af205750de791165659f76
yulongphp <604375988@qq.com>
2019-06-12 01:41:50 +0800

    Site updated: 2019-06-12 01:41:50

---
+++ js/tongji.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    /**
+     * 上报用户信息到打点服务器
+     */
+    $.get("http://exampletongji.com/index/tongji",
+        {
+            "time"  : gettime(),
+            "url"   : geturl(),
+            "refer" : getrefer(),
+            "ua"    : getuser_agent()
+        })
+});
+
+/*
+gettime(); //js获取当前时间
+getip(); //js获取客户端ip
+geturl(); //js获取客户端当前url
+getrefer(); //js获取客户端当前页面的上级页面的url
+getuser_agent(); //js获取客户端类型
+getcookie() //js获取客户端cookie
+*/
+
+function gettime(){
+    var nowDate = new Date();
+    return nowDate.toLocaleString();
+}
+function geturl(){
+    return window.location.href;
+}
+function getrefer(){
+    return document.referrer;
+}
+function getuser_agent(){
+    return navigator.userAgent;
+}
